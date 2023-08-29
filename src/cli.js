e1f822933d81ed36b1fec07524c69366eb5cdeb3
Fabrizio El Ajouri Romano <fabrizio.el@gmail.com>
2023-08-29 15:36:54 -0300

    new commit

--- src/cli.js
+++ src/cli.js
@@ -1,9 +1,28 @@
 import pegaArquivo from "./index.js";
-
+import fs from 'fs';
 const caminho = process.argv;
 
-async function processaTexto(caminho) {
-    const resultado = await pegaArquivo(caminho[2])
-    console.log("lista de arquivos", resultado);
+async function processaTexto(argumentos) {
+
+    const caminho = argumentos[2]
+    
+    if(fs.lstatSync(caminho).isFile()){
+        const resultado = await pegaArquivo(caminho)
+        console.log("lista de arquivos", resultado);
+    
+    } else if(fs.lstatSync(caminho).isDirectory()){
+        
+        const arquivos = await fs.promises.readdir(caminho)
+        
+        arquivos.forEach(async (nomeDeArquivo) => {
+            
+            const lista = await pegaArquivo(`${caminho}/${nomeDeArquivo}`)
+            
+            console.log(`${caminho}/${nomeDeArquivo}`)
+            console.log(lista)
+        })
+        // console.log(arquivos)
+        
+    }
 }
 processaTexto(caminho);
